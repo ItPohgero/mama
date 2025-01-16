070aab633124db0b10bf26dfdaf06a82bd64c60d
wahyu agus arifin <>
2025-01-16 19:47:10 +0700

    banner

--- src/modules/banner.ts
+++ src/modules/banner.ts
@@ -5,7 +5,10 @@ const banner = (): void => {
 	const title = chalk.blue(
 		figlet.textSync("mama", { horizontalLayout: "full" }),
 	);
-	const desc = chalk.green("Mama CLI : Like a mama who helps her child grow.");
+	const desc = chalk.green(
+		"Mama CLI: Like a mama who helps her child grow. :) Built with care by the Mataramandev community and wahyu a. arifin.",
+	);
+
 	console.log(title);
 	console.log(desc);
 };

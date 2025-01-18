e7788e67d7e6d2d03c8592e08141075ee86a7bb2
wahyu agus arifin <>
2025-01-18 13:28:09 +0700

    ma

--- src/cmd/next/make/make:hooks.ts
+++ src/cmd/next/make/make:hooks.ts
@@ -1,7 +1,6 @@
 import path from "node:path";
 import { useMake } from "@/hooks/use_make";
 
-// makeHooks.ts
 const HOOKS_TEMPLATE = path.join(
 	__dirname,
 	"../../../template/next/init/make/hooks.ejs.t",
